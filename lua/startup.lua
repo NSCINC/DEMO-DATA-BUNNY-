832027ff5c027e651039cf1b47d8192f095990bc
nsc i.o <158188835+NSCINC@users.noreply.github.com>
2024-10-20 15:31:04 -0300

    Add files via upload

---
+++ lua/startup.lua
@@ -0,0 +1,5 @@
+-- Inicialização do sistema
+dofile("ion_control.lua")
+dofile("hypertracing.lua")
+dofile("telemetry_handler.lua")
+composer create-project --prefer-dist laravel/laravel data bunny 
---
+++ swift/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BunnyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IonControlView() // A view principal do aplicativo
+        }
+    }
+}
